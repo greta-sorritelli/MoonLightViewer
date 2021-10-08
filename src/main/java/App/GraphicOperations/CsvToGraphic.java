f3ae2295f6d8f50c06ae4f3e800bce5048b5a14c
greta-sorritelli <greta.sorritelli@studenti.unicam.it>
2021-10-08 17:49:17 +0200

    file chooser

--- src/main/java/App/GraphicOperations/CsvToGraphic.java
+++ src/main/java/App/GraphicOperations/CsvToGraphic.java
@@ -9,53 +9,28 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class CsvToGraphic {
-    public static void main(String[] args)
-    {
+
+    File file = null;
+
+    public CsvToGraphic(File file) {
+        this.file = file;
+    }
+
+    public void readCsv(File file) {
         CSVReader reader = null;
-        try
-        {
-//parsing a CSV file into CSVReader class constructor
-        //    reader = new CSVReader(new FileReader("C:\\Users\\Greta\\Documents\\Greta\\università\\project\\esercizi\\traajectory.csv"));
-//            File file = new File ("C:\\<directory>");
-//            Desktop desktop = Desktop.getDesktop();
-            //Create a file chooser
-            final JFileChooser fc = new JFileChooser();
-
-//In response to a button click:
-//            int returnVal = fc.showOpenDialog(aComponent);
-//            reader = new CSVReader(new FileReader(file));
-
+        try {
+            reader = new CSVReader(new FileReader(file));
             String [] nextLine;
-//reads one line at a time
-            while ((nextLine = reader.readNext()) != null)
-            {
-                for(String token : nextLine)
-                {
-                    System.out.print(token);
+
+            while ((nextLine = reader.readNext()) != null) {
+                for(String token : nextLine) {
+                    System.out.print(token + " ");
                 }
                 System.out.print("\n");
             }
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-//    private void OpenActionPerformed(java.awt.event.ActionEvent evt) {
-//        int returnVal = fileChooser.showOpenDialog(this);
-//        if (returnVal == JFileChooser.APPROVE_OPTION) {
-//            File file = fileChooser.getSelectedFile();
-//            try {
-//                // What to do with the file, e.g. display it in a TextArea
-//                textarea.read( new FileReader( file.getAbsolutePath() ), null );
-//            } catch (IOException ex) {
-//                System.out.println("problem accessing file"+file.getAbsolutePath());
-//            }
-//        } else {
-//            System.out.println("File access cancelled by user.");
-//        }
-//    }
-
-
 }
--- src/main/java/javaFX/HomeController.java
+++ src/main/java/javaFX/HomeController.java
@@ -1,11 +1,36 @@
 package javaFX;
 
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import App.GraphicOperations.CsvToGraphic;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import javax.swing.*;
 
 public class HomeController {
 
-    public void openExplorer() {
-        
-    }
+    @FXML
+    private AnchorPane anchorID;
 
+    @FXML
+    private void openExplorer() {
+        FileChooser fileChooser = new FileChooser();
+        Stage stage = (Stage) anchorID.getScene().getWindow();
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            String path = file.getAbsolutePath();
+        }
+        CsvToGraphic read = new CsvToGraphic(file);
+        read.readCsv(file);
+    }
 }
+
+
