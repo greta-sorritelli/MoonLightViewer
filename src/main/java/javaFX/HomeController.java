eacb5d095396385340de4f3443bc30978bb7bb7a
clarissa-albanese <clarissa.albanese@studenti.unicam.it>
2021-10-11 17:19:01 +0200

    gitignore

--- src/main/java/javaFX/HomeController.java
+++ src/main/java/javaFX/HomeController.java
@@ -1,15 +1,9 @@
 package javaFX;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-
-import App.GraphicOperations.CsvToGraphic;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 import javafx.fxml.FXML;
 import javafx.geometry.Side;
-import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
@@ -17,6 +11,10 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class HomeController {
 
@@ -42,8 +40,6 @@ public class HomeController {
         }
     }
 
-
-
     public void getSeriesFromCsv(String path) {
         yAxis = new NumberAxis(-1, 1, 2);
         yAxis.setLabel("Values");
@@ -52,9 +48,9 @@ public class HomeController {
         lineChart.setLegendSide(Side.RIGHT);
         lineChart.setAnimated(true);
 
-        XYChart.Series xSeries = new XYChart.Series();
-        XYChart.Series ySeries = new XYChart.Series();
-        XYChart.Series zSeries = new XYChart.Series();
+        XYChart.Series<String,Number> xSeries = new XYChart.Series<>();
+        XYChart.Series<String,Number> ySeries = new XYChart.Series<>();
+        XYChart.Series<String,Number> zSeries = new XYChart.Series<>();
 
         xSeries.setName("X");
         ySeries.setName("Y");
@@ -64,22 +60,18 @@ public class HomeController {
             String[] nextLine;
             while ((nextLine = dataReader.readNext()) != null) {
                 if(nextLine.length == 0 || nextLine[0].equals("")) continue;
-                String time = String.valueOf(nextLine[0]);
+                String time = nextLine[0];
                 double X = Double.parseDouble(nextLine[1]);
-                xSeries.getData().add(new XYChart.Data(time, X));
+                xSeries.getData().add(new XYChart.Data<>(time, X));
                 double Y = Double.parseDouble(nextLine[2]);
-                ;
-                ySeries.getData().add(new XYChart.Data(time, Y));
+                ySeries.getData().add(new XYChart.Data<>(time, Y));
                 double Z = Double.parseDouble(nextLine[3]);
-                zSeries.getData().add(new XYChart.Data(time, Z));
+                zSeries.getData().add(new XYChart.Data<>(time, Z));
             }
         } catch (IOException | CsvValidationException e) {
             e.printStackTrace();
         }
-
         lineChart.getData().addAll(xSeries, ySeries, zSeries);
-//        Scene scene = new Scene(lineChart, 500, 400);
-
     }
 
 }
