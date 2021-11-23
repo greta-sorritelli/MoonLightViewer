c793e6aaa1807de20895f1dbaf0ea588f7ac1d7f
clarissa-albanese <clarissa.albanese@studenti.unicam.it>
2021-11-23 11:18:35 +0100

    update test

--- src/test/java/App/javaController/SimpleGraphControllerTest.java
+++ src/test/java/App/javaController/SimpleGraphControllerTest.java
@@ -1,24 +1,45 @@
 package App.javaController;
 
+import org.graphstream.graph.Graph;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleGraphControllerTest {
 
+    GraphController graphController = SimpleGraphController.getInstance();
+
     @Test
-    void createNodesVector() {
+    void createNodesVectorTest() {
     }
 
     @Test
-    void createPositions() {
+    void createPositionsTest() throws IOException {
+        File file = new File("src/main/resources/test/test.tra");
+        graphController.createGraphFromFile(file);
+        String line = "0,3,17,1,0,0,14,19,3,0,0";
+        graphController.createPositions(line);
+        Graph graph = graphController.getStaticGraph();
+        String id = String.valueOf(0);
+        String id1 = String.valueOf(1);
+        assertTrue(graph.getNode(id).hasAttribute("x"));
+        assertTrue(graph.getNode(id1).hasAttribute("x"));
+        assertTrue(graph.getNode(id).hasAttribute("y"));
+        assertTrue(graph.getNode(id1).hasAttribute("y"));
+        assertEquals(graph.getNode(id).getAttribute("x"),"3");
+        assertEquals(graph.getNode(id1).getAttribute("x"),"14");
+        assertEquals(graph.getNode(id).getAttribute("y"),"17");
+        assertEquals(graph.getNode(id1).getAttribute("y"),"19");
     }
 
     @Test
-    void getNodesValues() {
+    void getNodesValuesTest() {
     }
 
     @Test
-    void createGraphFromFile() {
+    void createGraphFromFileTest() {
     }
 }
