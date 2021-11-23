36a09d6bd33b0010b2496d88c1c964f391bdfe10
clarissa-albanese <clarissa.albanese@studenti.unicam.it>
2021-11-23 12:58:38 +0100

    update test

--- src/test/java/App/javaController/SimpleGraphControllerTest.java
+++ src/test/java/App/javaController/SimpleGraphControllerTest.java
@@ -1,16 +1,23 @@
 package App.javaController;
 
+import App.javaModel.graph.GraphType;
+import App.javaModel.graph.TimeGraph;
 import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SimpleGraphControllerTest {
 
     GraphController graphController = SimpleGraphController.getInstance();
+    List<TimeGraph> timeGraphList = new ArrayList<>();
 
     @Test
     void createNodesVectorTest() {
@@ -18,7 +25,7 @@ class SimpleGraphControllerTest {
 
     @Test
     void createPositionsTest() throws IOException {
-        File file = new File("src/main/resources/test/test.tra");
+        File file = new File("src/main/resources/test/static.tra");
         graphController.createGraphFromFile(file);
         String line = "0,3,17,1,0,0,14,19,3,0,0";
         graphController.createPositions(line);
@@ -40,6 +47,20 @@ class SimpleGraphControllerTest {
     }
 
     @Test
-    void createGraphFromFileTest() {
+    void createGraphFromFileTest() throws IOException {
+        File file = new File("src/main/resources/test/static.tra");
+        GraphType graphType = graphController.createGraphFromFile(file);
+        assertEquals(GraphType.STATIC, graphType);
+        graphController.setGraphList(timeGraphList);
+        File file1 = new File("src/main/resources/test/dynamic.tra");
+        GraphType graphType1 = graphController.createGraphFromFile(file1);
+        assertEquals(GraphType.DYNAMIC, graphType1);
+        assertEquals(1, graphController.getGraphList().size());
+        TimeGraph graph = graphController.getGraphList().get(0);
+        assertEquals(5,graph.getGraph().getNodeCount());
+        Node n0 = graphController.getGraphList().get(0).getGraph().getNode(String.valueOf(0));
+        Node n1 = graphController.getGraphList().get(0).getGraph().getNode(String.valueOf(1));
+        assertEquals(n0,graphController.getGraphList().get(0).getGraph().getEdge("id" + 1).getSourceNode());
+        assertEquals(n1,graphController.getGraphList().get(0).getGraph().getEdge("id" + 1).getTargetNode());
     }
 }
